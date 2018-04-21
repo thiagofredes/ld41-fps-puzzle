699b6b283522d4faf21a78fd4b1f9c53aae12e71
Thiago <trodrigues02@gmail.com>
2018-04-21 09:49:40 -0300

    Preliminar movement mechanics

---
+++ Assets/Scripts/FPSCamera.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : BaseGameObject
+{
+
+    public static Vector3 CameraForwardVectorOnGround
+    {
+        get
+        {
+            Vector3 forwardOnGround = _instance.transform.forward;
+            forwardOnGround.y = 0f;
+            return forwardOnGround;
+        }
+    }
+
+    public static Vector3 CameraRightVectorOnGround
+    {
+        get
+        {
+            Vector3 rightOnGround = _instance.transform.right;
+            rightOnGround.y = 0f;
+            return rightOnGround;
+        }
+    }
+
+    public float sensitivityX = 10f;
+
+    public float sensitivityY = 10f;
+
+    public float maxAngleY = 70f;
+
+    public float minAngleY = 70f;
+
+    public bool invertAxisY = false;
+
+    private float _currentX;
+
+    private float _currentY;
+
+    private static FPSCamera _instance;
+
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gamePaused && !gameEnded)
+        {
+            float horizontal = Input.GetAxis("Mouse X"); // deltas
+            float vertical = Input.GetAxis("Mouse Y");
+
+            _currentX += sensitivityX * horizontal * Time.deltaTime;
+            _currentY += sensitivityY * vertical * Time.deltaTime;
+        }
+    }
+
+    void LateUpdate()
+    {
+        _currentY = Mathf.Clamp(_currentY, minAngleY, maxAngleY);
+        this.transform.localRotation = Quaternion.Euler(
+                                        (invertAxisY ? -1f : 1f) * _currentY,
+                                        _currentX, 0f);
+    }
+}
---
+++ Assets/Scripts/FPSPlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSPlayerController : BaseGameObject {	
+
+	public CharacterController characterController;
+
+	public float movementSpeed;
+
+	public float gravity;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float forward = Input.GetAxis("Vertical");
+		float sidestep = Input.GetAxis("Horizontal");
+		Vector3 movement = forward * FPSCamera.CameraForwardVectorOnGround + sidestep * FPSCamera.CameraRightVectorOnGround;
+
+		characterController.Move(Time.deltaTime * (movementSpeed * movement - gravity * Vector3.up));
+	}
+}
