d0dbc45442bdd6590e40c5885f4027d7ffec354a
Thiago <trodrigues02@gmail.com>
2018-04-21 14:34:31 -0300

    Preliminar shooting mechanics.

--- Assets/Scripts/Player/FPSPlayerController.cs
+++ Assets/Scripts/Player/FPSPlayerController.cs
@@ -2,28 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSPlayerController : BaseGameObject {	
+public class FPSPlayerController : BaseGameObject
+{
 
-	public CharacterController characterController;
+    public CharacterController characterController;
 
-	public float movementSpeed;
+	public ShootController shootController;
 
-	public float gravity;
+    public float movementSpeed;
+
+    public float gravity;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float forward = Input.GetAxis("Vertical");
-		float sidestep = Input.GetAxis("Horizontal");
-		Vector3 movement = forward * FPSCamera.CameraForwardVectorOnGround + 
-							sidestep * FPSCamera.CameraRightVectorOnGround;
+    // Use this for initialization
+    void Start()
+    {
 
-		transform.forward = FPSCamera.CameraForwardVectorOnGround;
-		characterController.Move(Time.deltaTime * (movementSpeed * movement - gravity * Vector3.up));
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forward = Input.GetAxis("Vertical");
+        float sidestep = Input.GetAxis("Horizontal");
+        Vector3 movement = forward * FPSCamera.CameraForwardVectorOnGround +
+                            sidestep * FPSCamera.CameraRightVectorOnGround;
+
+        transform.forward = FPSCamera.CameraForwardVectorOnGround;
+
+        if (Input.GetMouseButton(0)){
+			shootController.Shoot();
+		}
+
+            characterController.Move(Time.deltaTime * (movementSpeed * movement - gravity * Vector3.up));
+    }
 }
---
+++ Assets/Scripts/Player/ShootController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour {
+	
+	public Transform shotOrigin;
+
+	public float timeBetweenShots;
+
+	public float bulletSpeed;
+
+	private float nextShotTime;
+
+	void Awake(){
+		nextShotTime = 0f;
+	}
+	public void Shoot(){
+		if(Time.time >= nextShotTime){
+			nextShotTime = Time.time + timeBetweenShots;
+			StartCoroutine(BulletCoroutine());
+		}
+	}
+
+	private IEnumerator BulletCoroutine(){
+		Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width/2f, Screen.height/2f));
+		RaycastHit raycastHit;
+		YieldInstruction endOfFrame = new WaitForEndOfFrame();		
+
+		while(!Physics.Raycast(ray.origin, ray.direction, out raycastHit, bulletSpeed, ~LayerMask.GetMask("Player", "Weapon"))){
+			yield return endOfFrame;
+			ray.origin = ray.origin + ray.direction.normalized * bulletSpeed;
+			Debug.DrawRay(ray.origin, ray.direction * bulletSpeed, Color.magenta);
+		}
+
+		EnemyController enemy = raycastHit.transform.GetComponent<EnemyController>();
+		if(enemy != null){
+			Debug.Log("Shot enemy " + enemy.name);
+		}
+		else{
+			Debug.Log("Collision with " + raycastHit.transform.name);
+		}
+	}	
+}
